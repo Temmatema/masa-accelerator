44a5c5782dcced52f61d613ac5c2105f454c2726
Temmatema <112764243+Temmatema@users.noreply.github.com>
2023-08-25 15:34:47 +0300

    fix bugs

--- source/js/modules/hero-swiper.js
+++ source/js/modules/hero-swiper.js
@@ -7,14 +7,26 @@ const swiper = new Swiper('.hero', {
   initialSlide: 0,
   slidesPerView: 1,
   slidesPerGroup: 1,
-  // autoplay: {
-  //   delay: 3000,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
   loop: true,
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
     clickable: true,
   },
+});
+
+const heroLinks = document.querySelectorAll('.hero__link');
+
+heroLinks.forEach((link, index) => {
+  link.addEventListener('focus', () => {
+    swiper.slideTo(index);
+  });
 });
